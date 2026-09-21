61e2cc25698cf69e9f07ff4675d7f9393fce5a0d
Recipes For Software <recipesforsoftware@gmail.com>
2026-09-21 02:21:27 +0200

    refactor: modernize Desktop app module (#63)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.compose.multiplatform) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
